a9b38be151a1847e002b089a732cb11849e618e4
chigskonkwo <67964333+chigskonkwo@users.noreply.github.com>
2021-02-28 19:00:38 -0500

    Update liverLRT_Mfuzz_PCA.R

--- Cluster Analyses, PCA, & Heatmap/liverLRT_Mfuzz_PCA.R
+++ Cluster Analyses, PCA, & Heatmap/liverLRT_Mfuzz_PCA.R
@@ -240,9 +240,9 @@ dataAllTimes <- column_to_rownames(dataJoin7, var = "geneNames") # Remove gene I
 dataAllTimesMat <- as.matrix(dataAllTimes)  
 
 liver_eset <- ExpressionSet(assayData = dataAllTimesMat)
-
 mest <- mestimate(liver_eset)
 
+#Run Mfuzz algorithm
 dataFuzz <- mfuzz(liver_eset,10,mest)
 
 #Genes within the core of the cluster, with a membership score greater than 0.4
